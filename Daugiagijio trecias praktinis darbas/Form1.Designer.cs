1695beeb24d767082388b34c85aef59a1daf1474
Dainoras Žiukas <65944827+dainoras99@users.noreply.github.com>
2021-05-15 20:21:31 +0300

    Finished
    Finished

---
+++ Daugiagijio trecias praktinis darbas/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Daugiagijio_trecias_praktinis_darbas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFolderButton = new System.Windows.Forms.Button();
+            this.arffCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.checkAllButton = new System.Windows.Forms.Button();
+            this.combineButton = new System.Windows.Forms.Button();
+            this.uncheckAllButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.continueButton = new System.Windows.Forms.Button();
+            this.checkLabel = new System.Windows.Forms.Label();
+            this.progressBarForCheck = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // openFolderButton
+            // 
+            this.openFolderButton.Location = new System.Drawing.Point(49, 31);
+            this.openFolderButton.Name = "openFolderButton";
+            this.openFolderButton.Size = new System.Drawing.Size(75, 23);
+            this.openFolderButton.TabIndex = 0;
+            this.openFolderButton.Text = "Open Folder";
+            this.openFolderButton.UseVisualStyleBackColor = true;
+            this.openFolderButton.Click += new System.EventHandler(this.openFolderButton_Click);
+            // 
+            // arffCheckedListBox
+            // 
+            this.arffCheckedListBox.FormattingEnabled = true;
+            this.arffCheckedListBox.Location = new System.Drawing.Point(49, 83);
+            this.arffCheckedListBox.Name = "arffCheckedListBox";
+            this.arffCheckedListBox.Size = new System.Drawing.Size(548, 244);
+            this.arffCheckedListBox.TabIndex = 1;
+            // 
+            // checkAllButton
+            // 
+            this.checkAllButton.Location = new System.Drawing.Point(227, 333);
+            this.checkAllButton.Name = "checkAllButton";
+            this.checkAllButton.Size = new System.Drawing.Size(75, 23);
+            this.checkAllButton.TabIndex = 2;
+            this.checkAllButton.Text = "Check All";
+            this.checkAllButton.UseVisualStyleBackColor = true;
+            this.checkAllButton.Visible = false;
+            this.checkAllButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // combineButton
+            // 
+            this.combineButton.Enabled = false;
+            this.combineButton.Location = new System.Drawing.Point(270, 373);
+            this.combineButton.Name = "combineButton";
+            this.combineButton.Size = new System.Drawing.Size(75, 23);
+            this.combineButton.TabIndex = 3;
+            this.combineButton.Text = "Combine";
+            this.combineButton.UseVisualStyleBackColor = true;
+            this.combineButton.Click += new System.EventHandler(this.combineButton_Click);
+            // 
+            // uncheckAllButton
+            // 
+            this.uncheckAllButton.Location = new System.Drawing.Point(308, 333);
+            this.uncheckAllButton.Name = "uncheckAllButton";
+            this.uncheckAllButton.Size = new System.Drawing.Size(75, 23);
+            this.uncheckAllButton.TabIndex = 6;
+            this.uncheckAllButton.Text = "Uncheck All";
+            this.uncheckAllButton.UseVisualStyleBackColor = true;
+            this.uncheckAllButton.Visible = false;
+            this.uncheckAllButton.Click += new System.EventHandler(this.uncheckAllButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(21, 415);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(615, 23);
+            this.progressBar.TabIndex = 7;
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Enabled = false;
+            this.pauseButton.Location = new System.Drawing.Point(94, 457);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 8;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Enabled = false;
+            this.cancelButton.Location = new System.Drawing.Point(270, 457);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 9;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click_1);
+            // 
+            // continueButton
+            // 
+            this.continueButton.Enabled = false;
+            this.continueButton.Location = new System.Drawing.Point(462, 457);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(75, 23);
+            this.continueButton.TabIndex = 10;
+            this.continueButton.Text = "Continue";
+            this.continueButton.UseVisualStyleBackColor = true;
+            this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            // 
+            // checkLabel
+            // 
+            this.checkLabel.AutoSize = true;
+            this.checkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkLabel.Location = new System.Drawing.Point(192, 184);
+            this.checkLabel.Name = "checkLabel";
+            this.checkLabel.Size = new System.Drawing.Size(254, 24);
+            this.checkLabel.TabIndex = 12;
+            this.checkLabel.Text = "Checking for corrupted files...";
+            this.checkLabel.Visible = false;
+            // 
+            // progressBarForCheck
+            // 
+            this.progressBarForCheck.Location = new System.Drawing.Point(179, 211);
+            this.progressBarForCheck.Name = "progressBarForCheck";
+            this.progressBarForCheck.Size = new System.Drawing.Size(277, 14);
+            this.progressBarForCheck.TabIndex = 13;
+            this.progressBarForCheck.Visible = false;
+            this.progressBarForCheck.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 492);
+            this.Controls.Add(this.progressBarForCheck);
+            this.Controls.Add(this.checkLabel);
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.uncheckAllButton);
+            this.Controls.Add(this.combineButton);
+            this.Controls.Add(this.checkAllButton);
+            this.Controls.Add(this.arffCheckedListBox);
+            this.Controls.Add(this.openFolderButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFolderButton;
+        private System.Windows.Forms.CheckedListBox arffCheckedListBox;
+        private System.Windows.Forms.Button checkAllButton;
+        private System.Windows.Forms.Button combineButton;
+        private System.Windows.Forms.Button uncheckAllButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button continueButton;
+        private System.Windows.Forms.Label checkLabel;
+        private System.Windows.Forms.ProgressBar progressBarForCheck;
+    }
+}
+
---
+++ Daugiagijio trecias praktinis darbas/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Daugiagijio_trecias_praktinis_darbas
+{
+
+    public partial class Form1 : Form
+    {
+        ManualResetEvent manualResetEvent = new ManualResetEvent(true);
+        CancellationTokenSource cancellationTokenSource;
+        CancellationToken cancellationToken;
+        private string location = "";
+        List<string> arffAttributes = new List<string>();
+        List<string> arffData = new List<string>();
+        private int progressBarCount = 0;
+        private int progressBarCountForCheck = 0;
+        private static readonly Object lockToken = new Object();
+      //  private string tempString = "";
+        private bool exists2 = false;
+        List<string> arffListBoxItems;
+        List<string> arffCheckedListBoxItems;
+        delegate void Runnable();
+        int countElements = 0;
+        StreamWriter streamWriter;
+        List<string> files;
+        private bool close = false;
+        StringBuilder tempString = new StringBuilder();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFolderButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+
+            if (browserDialog.ShowDialog(this) == DialogResult.OK)
+                location = browserDialog.SelectedPath;
+            arffCheckedListBox.Items.Clear();
+            checkLabel.Visible = true;
+            progressBarCountForCheck = 0;
+            Thread checkThread = new Thread(checkIfFileReliable);
+            checkThread.Start();
+        }
+        private void progressBarCheckValue()
+        {
+            this.Invoke((Action)delegate
+            {
+                progressBarForCheck.Minimum = 0;
+                // progressBar.Maximum = Directory.GetFiles(location).Length;
+                progressBarForCheck.Maximum = files.Count;
+                progressBarForCheck.Value = 0;
+            });
+            while (progressBarForCheck.Maximum != progressBarCountForCheck)
+            {
+                this.Invoke((Action)delegate
+                {
+                    if (exists2 == true)
+                    {
+                        progressBarForCheck.Value = progressBarCountForCheck;
+                        exists2 = false;
+                    }
+                });
+
+            }
+        }
+        private void checkIfFileReliable()
+        {
+            files = new List<string>();
+            List<string> tempFiles = new List<string>();
+            List<string> tempAttributes = new List<string>();
+            var firstFile = Directory.GetFiles(location).FirstOrDefault();
+            string[] firstFileLenght = File.ReadAllLines(firstFile);
+
+            string data = firstFileLenght[firstFileLenght.Length - 1];
+            String[] oneFileArffData = data.Split(',');
+
+            tempFiles = Directory.GetFiles(location).OfType<string>().ToList();
+
+            foreach (string file in tempFiles)
+            {
+                string[] lines = File.ReadAllLines(file);
+                if (lines.Contains("@relation SMILEfeatures") && lines.Contains("@data"))
+                    files.Add(file);
+            }
+            string[] lines2 = File.ReadAllLines(firstFile);
+            foreach (string line2 in lines2)
+            {
+                if (line2.Contains("@attribute"))
+                {
+                    tempAttributes.Add(line2);
+                }
+            }
+            int i = 0;
+            this.Invoke((Action)delegate
+            {
+                progressBarForCheck.Visible = true;
+            });
+            Thread checkProgressBar = new Thread(progressBarCheckValue);
+            checkProgressBar.Start();
+            foreach (string file in files.ToArray())
+            {
+                progressBarCountForCheck++;
+                i++;
+                string[] liness = File.ReadAllLines(file);
+
+                string data2 = liness[liness.Length - 1];
+                String[] oneFileArffData2 = data2.Split(',');
+
+                if (liness.Length != firstFileLenght.Length)
+                    files.Remove(file);
+
+                if (oneFileArffData.Length != oneFileArffData2.Length)
+                    files.Remove(file);
+
+                    Console.WriteLine(i);
+                exists2 = true;
+            }
+            Thread.Sleep(700);
+            Thread addingAttributesThread = new Thread(addingAttributes);
+            addingAttributesThread.Start();
+            this.Invoke((Action)delegate
+            {
+                checkLabel.Visible = false;
+                progressBarForCheck.Visible = false;
+                progressBarForCheck.Value = 0;
+            });
+        }
+        private void addingAttributes()
+        {
+            // var file = Directory.GetFiles(location, "*.*").FirstOrDefault();
+            var firstFile = files.FirstOrDefault();
+            string[] lines2 = File.ReadAllLines(firstFile);
+            foreach (string line2 in lines2)
+                if (line2.Contains("@attribute"))
+                {
+                    this.Invoke((Action)delegate
+                    {
+                        arffCheckedListBox.Items.Add(line2);
+                    });
+                }
+            this.Invoke((Action)delegate
+            {
+                combineButton.Enabled = true;
+                checkAllButton.Visible = true;
+                uncheckAllButton.Visible = true;
+            });
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < arffCheckedListBox.Items.Count; i++)
+            {
+                arffCheckedListBox.SetItemChecked(i, true);
+            }
+        }
+
+        private void uncheckAllButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < arffCheckedListBox.Items.Count; i++)
+            {
+                arffCheckedListBox.SetItemChecked(i, false);
+            }
+        }
+
+        private void saveToFileOnce()
+        {
+            string fileText = "@relation SMILEfeatures\n\n";
+            foreach (string arffAttribute in arffAttributes)
+            {
+                fileText += arffAttribute + "\n";
+            }
+            fileText += "\n@data\n\n";
+            File.WriteAllText(@"D:\KOLEGIJOS MEDZIAGA\DAUGIAGIJIS PROGRAMAVIMAS\Daugiagijio trecias praktinis darbas\newFile.arff", fileText);
+            streamWriter = new StreamWriter(@"D:\KOLEGIJOS MEDZIAGA\DAUGIAGIJIS PROGRAMAVIMAS\Daugiagijio trecias praktinis darbas\newFile.arff", true);
+        }
+        private void combineButton_Click(object sender, EventArgs e)
+        {
+            close = true;
+            checkAllButton.Visible = false;
+            uncheckAllButton.Visible = false;
+            combineButton.Enabled = false;
+            pauseButton.Enabled = true;
+            continueButton.Enabled = true;
+            cancelButton.Enabled = true;
+            arffAttributes = new List<string>();
+            cancellationTokenSource = new CancellationTokenSource();
+            cancellationToken = cancellationTokenSource.Token;
+            Thread combineThread = new Thread(combine);
+            Thread progressBarThread = new Thread(() => progressBarValue(combineThread));
+            progressBarThread.Start();
+            combineThread.Start();
+
+        }
+        void combine()
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            this.Invoke((Action)delegate
+            {
+                foreach (string arffValue in arffCheckedListBox.CheckedItems)
+                {
+                    arffAttributes.Add(arffValue);
+                }
+                saveToFileOnce();
+            });
+
+            countElements = arffAttributes.Count;
+            this.Invoke((Action)delegate
+            {
+                arffListBoxItems = arffCheckedListBox.Items.OfType<string>().ToList();
+                arffCheckedListBoxItems = arffCheckedListBox.CheckedItems.OfType<string>().ToList();
+            });
+            foreach (string file in files)
+            {
+                progressBarCount++;
+                i = 0;
+                k = 0;
+                string data = "";
+                //string temptempString = "";
+                string[] lines = File.ReadAllLines(file);
+                data = lines[lines.Length - 1];
+                String[] oneFileArffData = data.Split(',');
+
+
+                foreach (string arffValue in arffListBoxItems)
+                {
+                    i++;
+                    if (arffCheckedListBoxItems.Contains(arffValue))
+                    {
+                        k++;
+                        foreach (string thisData in oneFileArffData)
+                        {
+                            j++;
+                            if (i == j)
+                            {
+                                j = 0;
+                                //temptempString += thisData + ",";
+                                tempString.Append(thisData + ",");
+                                break;
+                            }
+                        }
+                        if (k == countElements)
+                            break;
+                    }
+                }
+                // temptempString = temptempString.Substring(0, temptempString.Length - 1);
+                //   tempString += temptempString + Environment.NewLine;
+                tempString.Append(Environment.NewLine);
+                //exists = true;
+
+               // temptempString = "";
+                manualResetEvent.WaitOne();
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    arffListBoxItems = null;
+                    arffCheckedListBoxItems = null;
+                    streamWriter.Close();
+                    File.Delete("D:\\KOLEGIJOS MEDZIAGA\\DAUGIAGIJIS PROGRAMAVIMAS\\Daugiagijio trecias praktinis darbas\\newFile.arff");
+                    progressBarCount = 0;
+                    //tempString = "";
+                    tempString.Remove(0, tempString.Length);
+                    this.Invoke((Action)delegate
+                    {
+                        arffCheckedListBox.Items.Clear();
+                        MessageBox.Show("Combining is cancelled");
+                        combineButton.Enabled = false;
+                        pauseButton.Enabled = false;
+                        cancelButton.Enabled = false;
+                        continueButton.Enabled = false;
+                    });
+                    break;
+                }
+
+            }
+        }
+        private void progressBarValue(Thread thread)
+        {
+            progressBarSettings();
+            while (true)
+            {
+
+                this.Invoke((Action)delegate
+                {
+                    progressBar.Value = progressBarCount;
+                });
+
+                //this.Invoke((Action)delegate
+                //{
+                //    if (exists == true)
+                //    {
+                //        if (!cancellationToken.IsCancellationRequested)
+                //            streamWriter.WriteLine(tempString);
+                //        exists = false;
+                //    }
+                //});
+
+                if (!thread.IsAlive)
+                {
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        streamWriter.WriteLine(tempString);
+                        arffListBoxItems = null;
+                        arffCheckedListBoxItems = null;
+                        streamWriter.Close();
+                        Thread.Sleep(700);
+                        this.Invoke((Action)delegate { MessageBox.Show("Done"); });
+                        this.Invoke((Action)delegate
+                        {
+                            tempString.Remove(0, tempString.Length);
+                            progressBarCount = 0;
+                            progressBar.Value = 0;
+                            arffCheckedListBox.Items.Clear();
+                            combineButton.Enabled = false;
+                            pauseButton.Enabled = false;
+                            cancelButton.Enabled = false;
+                            continueButton.Enabled = false;
+                            close = false;
+                        });
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void progressBarSettings()
+        {
+            this.Invoke((Action)delegate
+            {
+                progressBar.Minimum = 0;
+                // progressBar.Maximum = Directory.GetFiles(location).Length;
+                progressBar.Maximum = files.Count;
+                progressBar.Value = 0;
+            });
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            manualResetEvent.Reset();
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            manualResetEvent.Set();
+        }
+
+
+        private void cancelButton_Click_1(object sender, EventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (close == true)
+            e.Cancel = true;
+        }
+    }
+}
